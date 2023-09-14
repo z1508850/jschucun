1244ebf431550e3eb8946da176d73be07e33991e
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-09-14 22:55:33 +0800

    Add files via upload

---
+++ sy.js
@@ -0,0 +1,153 @@
+<style>
+@font-face {
+      font-family: "MyFont";
+   src: url("https://cdn3.codesign.qq.com/screens/2023/09/14/xDP3923q4EY409jwlKp14/nhyawbve23fq764v/7fd2e13c-8889-4df9-8e16-940b065f4d1a.png") format("woff2"),
+           url("https://cdn3.codesign.qq.com/screens/2023/09/14/xDP3923q4EY409jwlKp14/nhyawbve23fq764v/e46ef2a7-10b7-4146-aee6-c3896d9c4175.png") format("woff");
+    }
+    body {
+      font-family: "MyFont", sans-serif;
+background-image: url('https://cdn3.codesign.qq.com/screens/previews/2023/09/14/DKmwMQqtORMzYDe7DEkt5kBwa6MpgIYYBu2FLI5Z.png');
+    }
+.h4{
+ font-family: "MyFont";
+   }
+.site__title {
+color: #f35626;
+font-family: MyFont;
+background-image: -webkit-linear-gradient(yellow, yellow);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+-webkit-animation: hue 1s infinite linear;
+}
+@-webkit-keyframes hue {
+from {
+
+-webkit-filter: hue-rotate(0deg);
+-moz-filter: hue-rotate(0deg);
+}
+to {
+-webkit-filter: hue-rotate(-360deg);
+-moz-filter: hue-rotate(-360deg);
+}
+}
+</style>
+
+<div style = "text-align : center;margin-top:20px;">
+
+
+
+
+<img id = "tuijiantu" style = "margin-top:0px;width:250px;" src = "https://cdn3.codesign.qq.com/screens/previews/2023/09/14/5KXa9YRFdwjOoxFI5sUZpYhdIPS85VvHouZiYdHU.png"   />
+
+</div>
+
+<script src="//lib.baomitu.com/jquery/1.12.4/jquery.min.js"></script>
+
+
+<h4>
+
+
+
+<hr>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red"  class = "shoujis" style='font-family: MyFont;'>【NE】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 单板GS 最强足本 可裸奔推荐搭配FF</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【BBC】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 把把C 稳定NE-自带BOT-支持BGX</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【传奇】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 自带BOT各种收费合集-稳定奔放</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【飒】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> —  稳定NE-原不知名</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【CJJ】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 原PS/PJ稳定奔放</font></b></li>
+
+
+<hr>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【GGBond】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE  BGX 目前最稳定周卡！</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【R8】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 支持 BGX</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【Redbird】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 周卡只能续费，月卡可以注册！稳定奔放大号 </font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【龍】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 只能续费-自带BOT 支持BGX </font></b></li>
+
+
+
+<hr>
+</h4>
+<style>
+  #mybutton{
+	background:transparent;	
+	border-width:0px;
+	outline:none;
+}
+
+</style>
+
+<div class="btn-group btn-group-justified">
+
+<a  target="_blank" class="btn btn-block btn-success"   style="background: rgba(135,206,250, 0.2); color:#999b9c ; border: 0px;" onclick="javascrtpt:window.open('https://lol.5v5.lol/');"><i class="fa fa-book"></i> 黑号地址</a>
+<a  target="_blank"  class="btn btn-block btn-success" style="background: rgba(135,206,250, 0.3); color: #999b9c; border: 0px;" onclick="javascrtpt:window.open('https://api.vvhan.com/api/qqChat?qq=2420873117');"><i class="fa fa-qq"></i> 联系客服</a>
+<a  target="_blank" class="btn btn-block btn-success"   style="background: rgba(0,255,255, 0.2); color:#999b9c ; border: 0px;" onclick="javascrtpt:window.open('https://qm.qq.com/cgi-bin/qm/qr?k=HFMl2Y_M02o6H-ZERPNqOFyh8oHdEvdE&jump_from=webapi&authKey=2nc7QfwHq35SE1eS38JyzV9cLRcdKI7l0dCrP0k6A1FwU3BAyz669QbBIzy+iGoO');"><i class="fa fa-comments"></i> 点击加群</a>
+<a  target="_blank"  class="btn btn-block btn-success" style="background: rgba(0,255,255, 0.3); color: #999b9c; border: 0px;" 
+  onclick="javascrtpt:window.open('https://xzwp.lol/');"><i class="fa fa-cloud-download"></i> 下载网盘</a>
+
+</div>
+<br>
+<script>
+            ((function() {
+            var callbacks = [],  timeLimit = 50, open = false;
+            setInterval(_debug, 1);
+            return {
+                addListener: function(fn) {
+                    callbacks.push(fn);
+                },
+                cancleListenr: function(fn) {
+                    callbacks = callbacks.filter(function(v) {
+                        return v !== fn;
+                    });
+                }
+            }
+ 
+            function _debug() {
+                var startTime = new Date();
+                debugger;
+                if (new Date() - startTime > timeLimit) {
+                    if (!open) {
+                        callbacks.forEach(function(fn) {
+                            fn.call(null);
+                        });
+                    }
+                    open = true;
+                    window.stop();
+                    alert('大佬别扒了 需要同款请邮件联系 lol@xzwp.lol');
+                    document.body.innerHTML = "";
+                } else {
+                    open = false;
+                }
+            }
+        })()).addListener(function() {
+            window.location.reload();
+        });
+</script>
+<script>
+var a = 'retrtrfdcfvvvv';
+  var ym = window.location;
+  var ym2 = 'cc.xbi5.cn/s/';
+  var ym3 = String(ym);
+  function suan(a){
+    var re = a.substring(0,2);
+    var tr = a.substring(2,4);
+    var tr2 = a.substring(4,6);
+    var fd = a.substring(6,8);
+    var cf = a.substring(8,10);
+    var vv = a.substring(10,12);
+    var vv2 = a.substring(12,14);
+    re = 'h';
+    tr = 't';
+    tr2 ='t';
+    fd = 'p';  
+    cf = ':';
+    vv = '/';
+    vv2 = '/';
+    var p = re+tr+tr2+fd+cf+vv+vv2;
+    return p;
+  }
+  if (ym3.indexOf(ym2) == -1 ) {
+ alert("防扒站 by 5v5 此页面专属于cc.xbi5.cn/s/ 需要同款美化请邮件联系lol@xzwp.lol"); 
+  
+    var b = suan(a) + 'cc.xbi5.cn/s/';
+    window.location = b;
+  }
+</script>
