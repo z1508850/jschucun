e27dac9f6c65edb55f61b3bab40ad88c1a15578f
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-09-15 03:35:48 +0800

    Add files via upload

---
+++ tc.js
@@ -0,0 +1,5 @@
+<div style="text-align: center; background-color: white;">
+<strong style="text-align:center;" ><span style="text-align:center;color:black;font-size:18px;"><strong><span  style="color:red;font-size:24px;  font-family: "MyFont";">永久域名：5v5.lol   防止迷路！</span></strong><br />
+</span></strong>
+</p>
+	<strong><span style="color:red;font-size:18px;  font-family: "MyFont";">不圈不跑 售后TOP1 最新售后QQ：2420873117【6.10更新】 <br><br>所有卡若是缺货请联系管理拿卡<br><br>推荐支付宝下单无手续费<br></div>
