f1450483ebfb94898303514a5f2b1ed22355c771
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-09-19 02:43:33 +0800

    Add files via upload

---
+++ kun/kun-sy.js
@@ -0,0 +1,219 @@
+<style>
+@font-face {
+      font-family: "MyFont";
+   src: url("https://cdn3.codesign.qq.com/screens/2023/09/14/xDP3923q4EY409jwlKp14/nhyawbve23fq764v/7fd2e13c-8889-4df9-8e16-940b065f4d1a.png") format("woff2"),
+           url("https://cdn3.codesign.qq.com/screens/2023/09/14/xDP3923q4EY409jwlKp14/nhyawbve23fq764v/e46ef2a7-10b7-4146-aee6-c3896d9c4175.png") format("woff");
+    }
+    body {
+      font-family: "MyFont", sans-serif;
+background-image: url('https://cdn3.codesign.qq.com/screens/previews/2023/09/14/DKmwMQqtORMzYDe7DEkt5kBwa6MpgIYYBu2FLI5Z.png');
+    }
+.h4{
+ font-family: "MyFont";
+   }
+.site__title {
+color: #f35626;
+font-family: MyFont;
+background-image: -webkit-linear-gradient(yellow, yellow);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+-webkit-animation: hue 1s infinite linear;
+}
+@-webkit-keyframes hue {
+from {
+
+-webkit-filter: hue-rotate(0deg);
+-moz-filter: hue-rotate(0deg);
+}
+to {
+-webkit-filter: hue-rotate(-360deg);
+-moz-filter: hue-rotate(-360deg);
+}
+}
+
+ .hr {
+    color: #000;
+    border: 0;
+    font-size: 12px;
+    padding: 10px 0;
+    position: relative;
+}
+.hr::before {
+font-weight: bold;
+font-family: "MyFont";
+    content: "天卡推荐";
+ font-size: 16px;
+    position: absolute;
+    padding: 0 10px;
+    line-height: 1px;
+    border: solid red;
+    border-width: 0 100vw;
+    white-space: nowrap;
+    left: 50%;
+    transform: translateX(-50%);
+}
+ .hrr {
+    color: 	#000;
+    border: 0;
+    font-size: 12px;
+    padding: 10px 0;
+    position: relative;
+}
+.hrr::before {
+font-weight: bold;
+font-family: "MyFont";
+    content: "周月推荐";
+ font-size: 16px;
+    position: absolute;
+    padding: 0 10px;
+    line-height: 1px;
+    border: solid Yellow;
+    border-width: 0 100vw;
+    white-space: nowrap;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+ .hrrr {
+    color: 	#000;
+    border: 0;
+    font-size: 12px;
+    padding: 10px 0;
+    position: relative;
+}
+.hrrr::before {
+font-weight: bold;
+font-family: "MyFont";
+    content: "售后无忧";
+ font-size: 16px;
+    position: absolute;
+    padding: 0 10px;
+    line-height: 1px;
+    border: solid Chartreuse;
+    border-width: 0 100vw;
+    white-space: nowrap;
+    left: 50%;
+    transform: translateX(-50%);
+}
+</style>
+
+<div style = "text-align : center;margin-top:20px;">
+
+
+
+
+<img id = "tuijiantu" style = "margin-top:0px;width:250px;" src = "https://cdn3.codesign.qq.com/screens/previews/2023/09/14/5KXa9YRFdwjOoxFI5sUZpYhdIPS85VvHouZiYdHU.png"   />
+
+</div>
+
+<script src="//lib.baomitu.com/jquery/1.12.4/jquery.min.js"></script>
+<script>
+            ((function() {
+            var callbacks = [],  timeLimit = 50, open = false;
+            setInterval(_debug, 1);
+            return {
+                addListener: function(fn) {
+                    callbacks.push(fn);
+                },
+                cancleListenr: function(fn) {
+                    callbacks = callbacks.filter(function(v) {
+                        return v !== fn;
+                    });
+                }
+            }
+ 
+            function _debug() {
+                var startTime = new Date();
+                debugger;
+                if (new Date() - startTime > timeLimit) {
+                    if (!open) {
+                        callbacks.forEach(function(fn) {
+                            fn.call(null);
+                        });
+                    }
+                    open = true;
+                    window.stop();
+                    alert('大佬别扒了 需要同款请邮件联系 lol@xzwp.lol');
+                    document.body.innerHTML = "";
+                } else {
+                    open = false;
+                }
+            }
+        })()).addListener(function() {
+            window.location.reload();
+        });
+</script>
+<script>
+var a = 'retrtrfdcfvvvv';
+  var ym = window.location;
+  var ym2 = 'bsq.iosds.com/s';
+  var ym3 = String(ym);
+  function suan(a){
+    var re = a.substring(0,2);
+    var tr = a.substring(2,4);
+    var tr2 = a.substring(4,6);
+    var fd = a.substring(6,8);
+    var cf = a.substring(8,10);
+    var vv = a.substring(10,12);
+    var vv2 = a.substring(12,14);
+    re = 'h';
+    tr = 't';
+    tr2 ='t';
+    fd = 'p';  
+    cf = ':';
+    vv = '/';
+    vv2 = '/';
+    var p = re+tr+tr2+fd+cf+vv+vv2;
+    return p;
+  }
+  if (ym3.indexOf(ym2) == -1 ) {
+ alert("防扒站 by 5v5 此页面专属于bsq.iosds.com/s/ 需要同款美化请邮件联系lol@xzwp.lol"); 
+  
+    var b = suan(a) + 'bsq.iosds.com/s';
+    window.location = b;
+  }
+</script>
+
+
+
+
+
+<hr class="hr">
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red"  class = "shoujis" style='font-family: MyFont;'>【NE】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 单板GS 最强足本 可裸奔推荐搭配FF</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【BBC】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 把把C 稳定NE-自带BOT-支持BGX</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【传奇】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 自带BOT各种收费合集-稳定奔放</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【飒】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> —  稳定NE-原不知名</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" class = "shoujis"  style='font-family: MyFont;'>【CJJ】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 原PS/PJ稳定奔放</font></b></li>
+
+
+<hr class="hrr">
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【GGBond】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE  BGX 目前最稳定周卡！</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【R8】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 稳定NE 支持 BGX</font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【Redbird】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 周卡只能续费，月卡可以注册！稳定奔放大号 </font></b></li>
+<li class="list-group-item tishi" style="background-color: rgba(255, 255, 255, 0); border:none;" ><b><font color="red" style='font-family: MyFont;'>【龍】</font><font color="black"  class = "shoujis" style='font-family: MyFont;'> — 只能续费-自带BOT 支持BGX </font></b></li>
+
+
+
+<hr class="hrrr">
+</h4>
+<style>
+  #mybutton{
+	background:transparent;	
+	border-width:0px;
+	outline:none;
+}
+
+</style>
+
+<div class="btn-group btn-group-justified">
+
+<a  target="_blank" class="btn btn-block btn-success"   style="background: rgba(135,206,250, 0.2); color:black ; border: 0px;" onclick="javascrtpt:window.open('https://vn.vmp.cc/vniao/1DAED77F');"><i class="fa fa-book"></i> 黑号地址</a>
+<a  target="_blank"  class="btn btn-block btn-success" style="background: rgba(135,206,250, 0.3); color: black; border: 0px;" onclick="javascrtpt:window.open('https://api.vvhan.com/api/qqChat?qq=84447955');"><i class="fa fa-qq"></i> 联系客服</a>
+
+<a  target="_blank"  class="btn btn-block btn-success" style="background: rgba(0,255,255, 0.3); color: black; border: 0px;" 
+  onclick="javascrtpt:window.open('https://xzwp.lol/');"><i class="fa fa-cloud-download"></i> 下载网盘</a>
+
+</div>
+<br>
+
+
---
+++ kun/kun-xd.js
@@ -0,0 +1,655 @@
+<div style="text-align : center"><img id="goumaitu" src="https://cdn3.codesign.qq.com/screens/2023/09/19/xDP3923q4EY409jwlKp19/g4oeqoupfcpoaj7h/ffc5c88c-23cd-428e-bcd6-a94aad526c26.png" style = "margin-top:0px;width:300px;"></div>
+
+<h4>
+<li  class = "list-group-item" style="background-color: rgba(255, 255, 255, 0); border:none; color:black;text-align:center; font-family: 'MyFont';"  ><b>下单购买商品后请保管好自己卡密，本站不定时清空购买记录 </b>
+</li>
+<li class="list-group-item" style="background-color: rgba(255, 255, 255, 0); border:none; color:black;text-align:center; font-family: 'MyFont';" > <b>本卡网保证卡密正确！保底的卡密可退可换，放心购买！</b>
+</li>
+<h4>
+
+
+
+
+<style type="text/css">
+ @keyframes rotate {
+        0% {
+          transform: rotate(0deg); /* 开始时不旋转 */
+        }
+        100% {
+          transform: rotate(360deg); /* 结束时旋转一周（360度） */
+        }
+      }
+
+.Vidage {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 0;
+background-image: url('https://cdn3.codesign.qq.com/screens/previews/2023/09/14/DKmwMQqtORMzYDe7DEkt5kBwa6MpgIYYBu2FLI5Z.png');
+}
+
+
+.block {
+
+
+
+   
+background-image:linear-gradient(to bottom right, #87CEFA, rgba(255,255,255,1));
+border:1px solid rgba(255,255,255,0.6);
+color:white;
+border-radius:10px;
+border-top-left-radius:10px;
+border-top-right-radius:10px;
+-webkit-box-shadow:0 0px 0 rgba(0, 0, 0, .5);
+box-shadow:0 0px 0 rgba(0, 0, 0, .5);
+
+}
+
+
+
+
+.Vidage, .Vidage__video {
+  min-width: 100%;
+  min-height: 100%;
+background=: "https://cdn3.codesign.qq.com/screens/previews/2023/09/14/DKmwMQqtORMzYDe7DEkt5kBwa6MpgIYYBu2FLI5Z.png"
+}
+
+
+.Vidage__video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: auto;
+  height: auto;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -o-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);/*display:none*/
+}
+
+.banner_bg {
+    padding: 60px 0 0;
+    min-width: 1500px;
+    position: relative;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+}
+
+.banner {
+  
+    
+    position: relative;
+    color: #fff;
+    border: 2px solid gold;
+    border-radius: 10px;
+    background: #ffd700;
+    transition: all .3s;
+	 width: 1540px;
+    margin: 0 auto;
+    padding: 30px;
+background-image: linear-gradient(to bottom right, 	LightSkyBlue , 	MediumPurple);
+    border: 1px solid rgba(255,255,255,0.6);
+  
+    border-radius: 10px;
+}
+
+.banner::before {
+    content: "";
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    right: -5px;
+    bottom: -5px;
+    border: 3px solid #9370DB;
+    border-radius: 10px;
+    animation: div5Ani 15s infinite linear;
+}
+
+.banner::after {
+    content: "";
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    right: -5px;
+    bottom: -5px;
+    border: 3px solid  #00BFFF;
+    border-radius: 10px;
+    animation: div5Ani 15s infinite linear;
+
+}
+
+@keyframes div5Ani {
+    0%,
+    100% {
+        clip-path: inset(0 0 98% 0);
+    }
+
+    25% {
+        clip-path: inset(0 98% 0 0);
+    }
+    50% {
+        clip-path: inset(98% 0 0 0);
+    }
+    75% {
+        clip-path: inset(0 0 0 98%);
+    }
+}
+
+.banner::after {
+    animation: div5Ani 15s infinite -7.5s linear;
+}
+
+.banner_right {
+    position: absolute;
+    bottom: 0;
+    background: url(https://cdn3.codesign.qq.com/screens/previews/2023/06/27/gPRKlwhghhCaCxi9bugXWXsybyzQ2yiFcFfJ3BvK.png) no-repeat;
+    width: 550px;
+    height: 400px;
+    background-size:100% 100%;
+    right: 80px;
+opacity: 0.6; 
+
+}
+
+.mjgg {
+    width: 800px;
+    height: 100%;
+    margin-left: -20px;
+    float: right;
+    color: #fff;
+   text-align: center;
+    position: absolute;
+user-select: none;
+}
+
+.ewm_bg {
+    float: right;
+    padding: 10px;
+    margin-right: 40px;
+    font-size: 14px;
+    color: rgba(255,255,255,.8);
+ 
+    position: relative;
+    right: 550px;
+}
+
+.ewm {
+    width: 140px;
+    height: 140px;
+    margin: 0 auto;
+    text-align: center;
+  
+}
+
+.clear {
+    clear: both;
+}
+.modal-content{
+background: rgb(86 85 85 / 33%);
+}
+
+
+.modal-footer button{
+
+background:rgb(38 255 24 / 50%);
+}
+
+.modal-header, .modal-footer {
+    padding-left: 20px;
+    padding-right: 20px;
+    background: rgb(255, 255, 255 ,1);
+    color: white;
+}
+.modal-title{
+background: rgb(255, 255, 255 ,1);
+color:black;
+font-family: "MyFont";
+font-weight:bold;
+}
+option{
+background:#1f1f1f;
+color:white;
+
+}
+.modal-body {
+    padding-left: 20px;
+    padding-right: 20px;
+    background: rgb(255, 255, 255 , 1);
+    color: white;
+
+}
+
+.input-group-addon{
+  font-family: "MyFont";
+background:rgba(0, 0, 0, 0.15);
+color:black;
+
+}
+
+.form-control{
+ font-family: "MyFont";
+background:rgba(0, 0, 0, 0.15);
+color:black;
+border-radius:0;
+
+}
+
+
+#alert_frame{
+
+display:none;
+
+}
+
+
+
+.layui-layer-rim{
+
+border-radius: 5px;
+background:rgba(255,255,255, 0.9);
+
+}
+
+
+.layui-layer-title{
+
+
+background:rgba(255, 255, 255, 1);
+border:0;
+color:black;
+
+}
+
+.layui-layer-dialog {
+    min-width: 260px;
+    background: rgba(255,255,255, 0.8);
+}
+
+
+.table>tbody>tr.info>td, .table>tbody>tr.info>th, .table>tbody>tr>td.info, .table>tbody>tr>th.info, .table>tfoot>tr.info>td, .table>tfoot>tr.info>th, .table>tfoot>tr>td.info, .table>tfoot>tr>th.info, .table>thead>tr.info>td, .table>thead>tr.info>th, .table>thead>tr>td.info, .table>thead>tr>th.info {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+
+table{
+           text-align: center;
+}
+
+th{
+           text-align: center;
+}
+
+.table-striped>tbody>tr:nth-of-type(odd) {
+    background: rgba(0, 0, 0, 0);
+    border: 0;
+}
+
+.table thead > tr > th, .table tbody > tr > th, .table tfoot > tr > th, .table thead > tr > td, .table tbody > tr > td, .table tfoot > tr > td, .table tbody + tbody, .table-bordered, .table-bordered > thead > tr > th, .table-bordered > tbody > tr > th, .table-bordered > tfoot > tr > th, .table-bordered > thead > tr > td, .table-bordered > tbody > tr > td, .table-bordered > tfoot > tr > td {
+    border-color: rgba(0, 0, 0, 0);
+}
+
+
+.label-success, .label-success[href]:hover, .label-success[href]:focus {
+    background-color: ＃FFFF99;
+    padding: 3.5px 5px;
+}
+
+.btn-info {
+    background-color: ＃87CEFA;
+    border-color: ＃87CEFA;
+}
+
+.btn-xs:hover{
+
+    background-color: pink;
+    border-color: pink;
+
+}
+
+.btn-xs::active {background-color: pink;
+    border-color: pink;}
+
+
+h1, h2, h3 {
+font-family: "MyFont";
+    margin-bottom: 15px;
+    color: black;
+}
+
+.layui-layer-content{
+color:black;
+}
+
+
+.btn-default {
+    background: rgba(255, 255, 255, 0.5);
+    border-color: #eeeeee;
+    border: 0;
+    color: black;
+}
+</style>
+
+
+
+<script>
+$(document).ready(function(){
+  
+var comer = document.domain;
+
+var system ={};  
+var p = navigator.platform;       
+system.win = p.indexOf("Win") == 0;  
+system.mac = p.indexOf("Mac") == 0;  
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);    
+
+
+
+
+if(system.win||system.mac||system.xll){
+
+
+$("#display_searchBar").hide();
+$(".navbar.navbar-inverse.navbar-fixed-top").hide();
+
+$(".row").css({"margin":"0 auto","width":"1544px"});
+$("#main-container").css({"margin-left":"5px","margin-top":"-60px"}); 
+
+
+
+
+$(".block-title").css({"border-bottom":"0","background": "rgba(0, 0, 0, 0)"});
+
+$(".alert.alert-info").css({"background":"rgba(0, 0, 0, 0.15)"});
+
+$(".alert.alert-info>p>span").css({"color":"white"});
+$(".alert.alert-info>p>span>span").css({"color":"white"});
+
+
+
+$($($("#query").children("div").get(1)).children().children("div").get(0)).children().css({"border-right":"0"});
+$($($("#query").children("div").get(1)).children().children("span").get(0)).hide();
+$($($("#query").children("div").get(1)).children().children("input").get(0)).css({"width":"635px"});
+
+$($($("#user-code2").children("div").get(0)).children("div").get(0)).css({"width":"90px","border-radius":"0"});
+$($($("#user-code2").children("div").get(0)).children("input").get(0)).css({"width":"635px"});
+
+
+
+
+
+$(".col-sm-6").css({"padding-left":"0","padding-right":"5px"});
+
+$(".input-group-addon").css({"background":"rgba(0, 0, 0, 0.15)","color":"black"});
+
+
+
+
+
+$(".list-group-item").css({"background-color":"rgba(0, 0, 0, 0)","border":"0"});
+
+
+$(".form-control").css({"background":"rgba(0, 0, 0, 0.15)","color":"black","border-radius":"0"});
+
+$(".input-group-btn").css({"width":"0"});
+$("#num_min").hide();
+$("#num_add").hide();
+
+
+$("#need").css({"color":"red","font-family":"MyFont"});
+
+
+
+$("#submit_cart_shop").css({"background":"#afde5c","color":"white","border":"0","font-family":"MyFont"});
+$("#submit_buy").css({"background":"#5ccdde","color":"white","border":"0","font-family":"MyFont"});
+$("#submit_query").css({"background":"#5ccdde","color":"white","border":"0","font-family":"MyFont"});
+
+$(".block.full").hide();
+
+
+
+
+
+$("#submit_cart_shop").hover(function(){
+   $("#submit_cart_shop").css({"background":"rgba(175,222,92, 0.8)","color":"white","border":"0"});
+},function(){
+    $("#submit_cart_shop").css({"background":"rgba(175,222,92, 0.7)","color":"white","border":"0"});
+});
+
+
+$("#submit_buy").hover(function(){
+   $("#submit_buy").css({"background":"rgba(92,205,222, 0.8)","color":"white","border":"0"});
+},function(){
+    $("#submit_buy").css({"background":"rgba(92,205,222, 0.7)","color":"white","border":"0"});
+});
+
+
+
+
+
+
+
+
+$("#sidebar").hide();
+
+$("body").prepend("<nav id = 'navbe'><div class='Vidage'><video id='VidageVideo' class='Vidage__video' preload='metadata' loop='' autoplay='' muted=''><source src='https://cdn3.codesign.qq.com/screens/previews/2023/09/14/DKmwMQqtORMzYDe7DEkt5kBwa6MpgIYYBu2FLI5Z.png' type='video/mp4'></video></div></nav>");
+
+   
+
+$("#navbe").before("<div class='banner_bg'><div class='banner'><div class='banner_right'></div><div class='mjgg'><a  target='_blank'   href='./user' ><img src='https://cdn3.codesign.qq.com/screens/previews/2023/09/19/eqkWO9RTjy90uoAY2eNCFMxhgR2o6naRVQDOKLJt.png' style='width:600px;'></a></div><div class='ewm_bg'><div class='ewm'><div id='qrcode' style='width:140px;height:140px;display: block;text-align: center;margin: 0 auto;' title='加我为好友吧！'><canvas width='140' height='140' style='display: none;'></canvas><a href='https://api.vvhan.com/api/qqChat?qq=84447955'><img alt='Your Image' style='animation: rotate 8s linear infinite; border-radius:140px; opacity: 0.6; 'src='https://cdn3.codesign.qq.com/screens/2023/09/19/xDP3923q4EY409jwlKp19/lb0se64cscwtuw36/aa82407d-78e7-4a9c-a5f0-362dd5949efb.png' style='display: block;'></a></div></div><br><b>最新售后Q：84447955<br>点击头像即可一键添加好友</b></div><div class='clear'></div></div></div>");
+
+$(".nav.nav-tabs").html("<li style='width: 50%;' align='center' class='active'><a href='#onlinebuy' style = 'background: rgba(0, 0, 0, 0);color:black;' data-toggle='tab' ><i class='fa fa-check-square-o'></i>&nbsp;<b>在线下单</b></a></li><li style='width: 50%;' align='center' ><a href='#query' style = 'background: rgba(0, 0, 0, 0);color:black;' data-toggle='tab' id='tab-query' ><i class='fa fa-search'></i>&nbsp;<b>查询订单</b></a></li>");
+
+$(".nav.nav-tabs").parent().before("<div class='block-title' style='border-bottom: 0px;font-family: MyFont;  color:black;  background: rgba(0, 0, 0, 0);'><h4 ><i class='fa fa-bullhorn'></i>&nbsp;<a style='font-family: MyFont; text-decoration:none;  color:black;'>商品选购 - 客服QQ：84447955</a></h4></div>");
+
+
+
+
+
+$($(".nav.nav-tabs").children("li").get(0)).css({"background": "rgba(0, 0, 0, 0.3)","cursor":"pointer","margin-left":"2.6%","width": "47.4%","border-radius":"5px"});
+$($(".nav.nav-tabs").children("li").get(1)).css({"background": "rgba(0, 0, 0, 0)","cursor":"pointer","margin-right":"2.6%","width": "47.4%","border-radius":"5px"});
+
+$($(".nav.nav-tabs").children("li").get(0)).click(function(){
+
+   $($(".nav.nav-tabs").children("li").get(0)).css({"background": "rgba(0, 0, 0, 0.15)"});
+   $($(".nav.nav-tabs").children("li").get(1)).css({"background": "rgba(0, 0, 0, 0)"});
+
+});
+
+$($(".nav.nav-tabs").children("li").get(1)).click(function(){
+
+   $($(".nav.nav-tabs").children("li").get(0)).css({"background": "rgba(0, 0, 0, 0)"});
+   $($(".nav.nav-tabs").children("li").get(1)).css({"background": "rgba(0, 0, 0, 0.15)"});
+
+});
+
+$("#num_min").css({"background": "rgba(0, 0, 0, 0.5)","border":"0"});
+$("#num_add").css({"background": "rgba(0, 0, 0, 0.5)","border":"0"});
+
+
+$(".list-group-item").parent().parent().attr("id","chaoshen");
+$("#chaoshen .block-title h4").html("<div style='font-family: MyFont;'> <i  style='color:black;' class='fa fa-bullhorn' ></i>&nbsp;  <a  style='color:black; text-decoration:none; ' class='boxtime' ></a></div>");
+
+
+$("#audio-play").before("<div class = 'footerrr' style='z-index:9999999999;position: relative;bottom: 20px;color: black;margin:0 auto;line-height:30px;'><div class='copyright' style = 'text-align:center;     width: 1540px;margin: 0 auto;padding: 30px;background-image: linear-gradient(to bottom right, rgba(255,255,255,1),	#87CEFA );border: 1px solid rgba(255,255,255,0.6);border-radius: 10px;' >Copyright © 2016-2023  <a href='https://mir.lol' tppabs='https://mir.lol' target='_blank'>mir.lol <span style = 'color:red;font-weight:bold;'>授权经销商</a></span></br>ICP备案/许可证号： <a href='http://beian.miit.gov.cn' tppabs='http://beian.miit.gov.cn/' target='_blank'><span style = 'color:red;font-weight:bold;'>赣ICP备2023001183号-2</span></a></div></div>");
+
+var my_audio={
+    obj:null,
+    is_auto_play:false,
+    is_loop:false,
+    set:function(a){
+        var self=this;
+        self.obj=new Audio(a.url);
+        if(a.auto){
+            self.is_auto_play=true;
+            self.play();
+        }
+        if(a.loop){
+            self.is_loop=true;
+        }
+    },
+    play:function(){
+        var self=this;
+        self.obj.play();
+        if(self.is_loop){
+            self.obj.onended = function() {
+                self.play();
+            };
+        }
+    },
+    pause:function(){
+        var self=this;
+        self.obj.pause();
+    },
+    stop:function(){
+        var self=this;
+        self.obj.pause();
+        self.obj.currentTime = 0.0; 
+    },
+    muted:function(){
+        var self=this;
+        self.obj.muted ? self.obj.muted = false : self.obj.muted = true; 
+    },
+    volume_add:function(){
+        var self=this;
+        self.obj.volume = self.obj.volume + 0.1; 
+    },
+    volume_minus:function(){
+        var self=this;
+        self.obj.volume = self.obj.volume - 0.85; 
+    },
+    init:function(a){
+        
+        var c = function(s) { 
+            var r={};
+            for (var k in s) {
+                r[k] = typeof s[k]==="object" ? c(s[k]) : s[k];
+            } 
+            return r; 
+        }
+        var self =  c(my_audio);
+        self.set(a);
+        return self;
+    }
+}
+var audio_bg = my_audio.init({url: "https://lol.5v5.lol/", auto: true});
+
+
+
+$("body").attr("onload","audio_bg.volume_minus()");
+
+}
+
+
+
+
+
+
+
+});
+
+
+
+var days=new  Array ("日", "一", "二", "三", "四", "五", "六");
+function showDT() {
+  var currentDT = new Date();
+  var y,m,date,day,hs,ms,ss,theDateStr;
+  y = currentDT.getFullYear(); 
+  m = (currentDT.getMonth()+1); 
+  date = currentDT.getDate(); 
+  day = currentDT.getDay(); 
+  theDateStr = y+"年"+  m +"月"+date+"日 星期"+days[day];
+  return theDateStr;
+  
+}
+</script>
+<script>
+       function getTime() {
+            var date = new Date();
+            var year = date.getFullYear(); //获取年份
+            var month = date.getMonth() + 1; //获取月份
+            var day = date.getDate(); //获取日期
+            var hour = date.getHours(); //获取小时
+            hour = hour < 10 ? '0' + hour : hour;
+            var minute = date.getMinutes(); // 获取分
+            minute = minute < 10 ? '0' + minute : minute;
+            var seconds = date.getSeconds(); //获取秒
+            var week = ["日", "一", "二", "三", "四", "五", "六"][date.getDay()];
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            return  '今日推荐 - '+year + '年' + month + '月' + day + '日&nbsp;' +'&nbsp星期' + week;
+        }
+ 
+        setInterval(function() {
+            document.querySelector('.boxtime').innerHTML = getTime();
+        }, 1000)
+    </script>
+<script>
+            ((function() {
+            var callbacks = [],  timeLimit = 50, open = false;
+            setInterval(_debug, 1);
+            return {
+                addListener: function(fn) {
+                    callbacks.push(fn);
+                },
+                cancleListenr: function(fn) {
+                    callbacks = callbacks.filter(function(v) {
+                        return v !== fn;
+                    });
+                }
+            }
+ 
+            function _debug() {
+                var startTime = new Date();
+                debugger;
+                if (new Date() - startTime > timeLimit) {
+                    if (!open) {
+                        callbacks.forEach(function(fn) {
+                            fn.call(null);
+                        });
+                    }
+                    open = true;
+                    window.stop();
+                    alert('大佬别扒了 需要同款请邮件联系 lol@xzwp.lol');
+                    document.body.innerHTML = "";
+                } else {
+                    open = false;
+                }
+            }
+        })()).addListener(function() {
+            window.location.reload();
+        });
+</script>
+<script>
+var a = 'retrtrfdcfvvvv';
+  var ym = window.location;
+  var ym2 = 'bsq.iosds.com/s';
+  var ym3 = String(ym);
+  function suan(a){
+    var re = a.substring(0,2);
+    var tr = a.substring(2,4);
+    var tr2 = a.substring(4,6);
+    var fd = a.substring(6,8);
+    var cf = a.substring(8,10);
+    var vv = a.substring(10,12);
+    var vv2 = a.substring(12,14);
+    re = 'h';
+    tr = 't';
+    tr2 ='t';
+    fd = 'p';  
+    cf = ':';
+    vv = '/';
+    vv2 = '/';
+    var p = re+tr+tr2+fd+cf+vv+vv2;
+    return p;
+  }
+  if (ym3.indexOf(ym2) == -1 ) {
+ alert("防扒站 by 5v5 此页面专属于bsq.iosds.com/s/ 需要同款美化请邮件联系lol@xzwp.lol"); 
+  
+    var b = suan(a) + 'bsq.iosds.com/s';
+    window.location = b;
+  }
+</script>
