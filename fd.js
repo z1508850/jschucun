c6fe8b58aaa98c97c85b81834db946dc07930472
z1508850 <73526298+z1508850@users.noreply.github.com>
2023-10-24 18:49:33 +0800

    Update fd.js

--- fd.js
+++ fd.js
@@ -1,6 +1,6 @@
-const myDiv = document.getElementById('myDiv');
+ const myDiv = document.getElementById('myDiv');
 
-  
+    
     const style = document.createElement('style');
     style.innerText = `
       .barrage {
@@ -10,22 +10,35 @@ const myDiv = document.getElementById('myDiv');
         transform: translate(-50%, -50%);
         background-color: #ffffffe6;
         color: #333;
-        padding: 10px 20px;
+        padding: 10px 15px;
         border-radius: 5px;
         z-index: 9999;
+        display: flex;
+        align-items: center;
+      }
+
+      .barrage-icon {
+        display: block;
+        width: 20px;
+        height: 20px;
+        background-image: url('https://cdn3.codesign.qq.com/screens/previews/2023/10/24/jM93UEfMyMIRXfy8jBGwHATwiF0mBZ3ngnmn0qcd.png');
+        background-size: cover;
+    margin-right: 4px;
+ margin-left: -5px;
+        line-height: 20px; 
       }
     `;
     document.head.appendChild(style);
 
     function showBarrage(event) {
-      event.preventDefault();
+      event.preventDefault(); 
 
       const barrage = document.createElement('span');
-      barrage.innerText = '为了您更好的游览体验 本站禁用右键。复制请使用 Ctrl+C';
+      barrage.innerHTML = '<span class="barrage-icon"></span>为了您更好的游览体验 本站禁用右键。复制请使用 Ctrl+C';
       barrage.classList.add('barrage');
       document.body.appendChild(barrage);
 
-      
+      // 将弹幕提示置于窗口中心
       const { clientWidth, clientHeight } = document.documentElement;
       barrage.style.left = `${clientWidth / 2}px`;
       barrage.style.top = `${clientHeight / 6}px`;
